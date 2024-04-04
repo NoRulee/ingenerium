17da2459afbd29d907fdba9e03db49643aaa2f85
Daniil <danyildyb@gmail.com>
2024-04-04 20:18:51 +0300

    fix: update schedule_page

--- lib/src/presentation/schedule_page.dart
+++ lib/src/presentation/schedule_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:injenerium_mobile_app/src/presentation/style/color_style.dart';
 import 'package:injenerium_mobile_app/src/presentation/style/text_stryle.dart';
 import 'package:injenerium_mobile_app/src/presentation/widgets/custom_app_bar.dart';
+import 'package:injenerium_mobile_app/src/presentation/widgets/custom_navigation_bar.dart';
 
 class SchedulePage extends StatelessWidget {
   const SchedulePage({super.key});
@@ -10,47 +11,62 @@ class SchedulePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: getSecondaryColor(context),
-      body: SafeArea(
+      body: const SafeArea(
         child: Column(
           children: [
-            const CustomAppBar(title: 'Расписание', settings: true),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: getBackgroundColor(context),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Биотехнологии',
-                        style: getRegularTextStyle(context),
-                      ),
-                      Text(
-                        'Время посещения',
-                        style: getLightTextStyle(context),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text(
-                              'data',
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+            CustomAppBar(title: 'Расписание', settings: true),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _CustomScheduleContainer(
+                      title: 'Биотехнологии',
+                      schedule: '· Понедельник, 12:30 \n· Вторник, 12:30 \n· Четверг, 14:00 \n· Суббота, 14:00 \n· Воскресенье, 14:00',
+                    ),
+                    _CustomScheduleContainer(
+                      title: 'Робототехника',
+                      schedule: '· Суббота, 9:00 \n· Воскресенье, 9:00',
+                    ),
+                  ],
                 ),
               ),
             ),
+            CustomNavBar(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomScheduleContainer extends StatelessWidget {
+  final String title;
+  final String schedule;
+
+  const _CustomScheduleContainer({required this.title, required this.schedule});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: getBackgroundColor(context),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: getRegularTextStyle(context)),
+              const SizedBox(height: 10.0),
+              Text('Время посещения', style: getLightTextStyle(context)),
+              const SizedBox(height: 10.0),
+              Text(schedule, style: getLightTextStyle(context)),
+            ],
+          ),
         ),
       ),
     );
